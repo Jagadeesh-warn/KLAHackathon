191324cbab7ed367f80457d9e728cd22fd81d879
jagadeeshrock15@gmail.com <21PD13@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 11:13:33 +0530

    prog3

--- level0/main.py
+++ level0/main.py
@@ -9,5 +9,8 @@ for item in data['neighbourhoods']:
     list1.append(data['neighbourhoods'][item]['distances'])
 
 list1.append(data['restaurants']['r0']['neighbourhood_distance'])
+list1[0].insert(0,0)
+for i in range(1,21):
+    list1[i].insert(0,list1[0][i])
 print(list1)
     
