02fdd56b788b28f648ffa72786720d154dea3ee1
jagadeeshrock15@gmail.com <21PD13@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 14:24:39 +0530

    Progress2

--- level1/main1.py
+++ level1/main1.py
@@ -2,5 +2,40 @@ import json
 from collections import deque
 
 
-file1 = open('C:\Users\TEMP.CS2K16\Downloads\level1a.json')
+file1 = open('C:\HackathoN\KLAHackathon\level1\level1a.json')
 data = json.load(file1)
+
+list1 = []
+length = data["n_neighbourhoods"]+len(data["restaurants"])
+
+list1.append([0]+data["restaurants"][data["vehicles"]["v0"]["start_point"]]["neighbourhood_distance"])
+for i in data["neighbourhoods"]:
+    list1.append([0]+data["neighbourhoods"][i]["distances"])
+print(list1)
+
+def travellingsalesman(c,cost,list2):
+    v=0
+    min=99999
+    visited[c]=1
+    list2.append("n%d"%(c-1))
+    for k in range(length):
+        if (list1[c][k] != 0) and (visited[k] == 0):
+            if list1[c][k] < min:
+                min=list1[c][k]
+                v=k
+    if min!=99999:
+        cost+=min
+    if v==0:
+        list2.append("r0")
+        cost+=list1[c][0]
+        return
+    travellingsalesman(v,cost,list2)
+
+visited=[0]*(length+1)
+cost=0
+list2=[]
+travellingsalesman(0,cost,list2)
+    
+list2[0]="r0"
+output={"v0":{"path":list2}}
+
