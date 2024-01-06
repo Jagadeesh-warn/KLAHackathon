ef685e41fa4da085e6a0dbda61777053d2f7017d
jagadeeshrock15@gmail.com <21PD13@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 11:02:26 +0530

    prog2

--- level0/main.py
+++ level0/main.py
@@ -4,5 +4,10 @@ file1 = open('C:\HackathoN\KLAHackathon\level0\level0.json')
 
 data = json.load(file1)
 
-for i in data['neighbourhoods']:
-    print(i)
+list1 = []
+for item in data['neighbourhoods']:
+    list1.append(data['neighbourhoods'][item]['distances'])
+
+list1.append(data['restaurants']['r0']['neighbourhood_distance'])
+print(list1)
+    
